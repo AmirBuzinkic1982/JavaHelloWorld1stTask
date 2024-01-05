3570b36b2325832fff4ee8e9c8897b9bbae3cebb
AmirBuzinkic1982 <133097866+AmirBuzinkic1982@users.noreply.github.com>
2024-01-05 12:45:55 +0100

    Update JavaHelloWorld.java

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,12 +1,12 @@
 JavaHelloWorld.JavaHelloWorld
 
 public class JavaHelloWorld{
-public static void main(String{}args){
+public static void main(String[]args){
 
 
     for(int i=0;i<36;i++){
 
-        System.out.println(x:"Hello World")
+        System.out.println("Hello World");
     }
 }
 }
